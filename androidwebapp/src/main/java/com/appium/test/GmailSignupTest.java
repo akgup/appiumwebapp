eb40215653279b30b710ba88bce64bc7e35d3c3a
akgup <akshay19vit@gmail.com>
2016-09-13 11:55:23 +0530

    adding comments

--- androidwebapp/src/main/java/com/appium/test/GmailSignupTest.java
+++ androidwebapp/src/main/java/com/appium/test/GmailSignupTest.java
@@ -1,31 +1,40 @@
 package com.appium.test;
 
 import java.net.MalformedURLException;
-import org.testng.AssertJUnit;
+
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
 import com.appium.pagebject.PageObject;
 
 public class GmailSignupTest {
 
+	// create class object to access selenium methos
 	PageObject pageObject = new PageObject();
 
 	@BeforeClass
 	public void setUp() throws MalformedURLException {
+		// set driver capabilities
 		pageObject.setCapabilities();
 	}
 
 	@Test
 	public void VerifyAlreadyTakenMailIdError() {
 
+		// open gmail signup page
 		pageObject.navigateToSingupPage();
+
+		// fill the form with required details
 		pageObject.fillUserDetails("akshay", "gupta", "akshay19vit", "abcd1234@1324", "abcd1234@1324", "7387152444",
 				"recover1234@gmail.com");
 
+		// capture actual error message shown on screen
 		String errmsg = pageObject.getTextById("errormsg_0_GmailAddress");
 
-		AssertJUnit.assertEquals(errmsg, "That username is taken. Try another.");
+		// verify actual vs expected
+		Assert.assertEquals(errmsg, "That username is taken. Try another.");
 
 	}
 
@@ -38,7 +47,7 @@ public class GmailSignupTest {
 
 		String errmsg = pageObject.getTextById("errormsg_0_PasswdAgain");
 
-		AssertJUnit.assertEquals("These passwords don't match. Try again?", errmsg);
+		Assert.assertEquals("These passwords don't match. Try again?", errmsg);
 
 	}
 
@@ -51,8 +60,7 @@ public class GmailSignupTest {
 
 		String errmsg = pageObject.getTextById("errormsg_0_RecoveryPhoneNumber");
 
-		AssertJUnit.assertEquals("This phone number format is not recognized. Please check the country and number.",
-				errmsg);
+		Assert.assertEquals("This phone number format is not recognized. Please check the country and number.", errmsg);
 
 	}
 
